19123b887a084f6e2bde9012e1adefa9cdac0469
james david <jamesistranho@gmail.com>
2021-06-14 02:08:13 -0300

    stars

---
+++ src/compoenents/Stars.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import Particles from 'react-particles-js'
+import particlesStyles from '../styles/stars'
+
+function Stars () {
+  return (
+    <Particles params={particlesStyles} />
+  )
+}
+
+export default Stars
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 
 import React from 'react'
 
+import Stars from '../compoenents/Stars'
 import '../styles/home.css'
 
 function Home () {
   return (
     <div className='home-container'>
+      <Stars />
       <div className='about'>
-        <h1>OH, Hello There!</h1>
+        <div className='hello'>
+          <h1>Oh, </h1>
+          <h2> Hello There! </h2>
+        </div>
         <span>I'm <span id='my-name' className='my-name'>James D. Dockal</span></span>
         <p>Fullstack Developer</p>
       </div>

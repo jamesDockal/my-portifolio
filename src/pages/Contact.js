74c2faa454f4f93e1fd565d8f034cd3e406f1002
james dockal <jamesistranho@gmail.com>
2021-07-01 07:23:09 -0300

    add animation onfocus input

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import SocialMedias from "../compoenents/SocialMedias";
 
@@ -9,19 +9,33 @@ import "../styles/contact.css";
 
 // 5B06A7
 
-const emailStyle = {
-  color: "#5B06A7",
-  fontSize: "5rem",
-  marginBottom: "auto",
-  cursor: "pointer",
-  margin: 0,
-};
-
 function Contact() {
+  const [toggleInput, settoggleInput] = useState(false);
+
+  const emailStyle = {
+    color: `${toggleInput ? "#5b06a7" : "#5f82d9"}`,
+    transition: "0.3s all ease",
+    fontSize: "5rem",
+    marginBottom: "auto",
+    cursor: "pointer",
+  };
+  // 5b06a7
+  // 5f82d9
+  function handleToggle() {
+    settoggleInput(!toggleInput);
+    console.log(toggleInput);
+  }
+
   return (
     <div className="contact-container">
       <div className="contact-about">
-        <strong>Contact me</strong>
+        <strong
+          style={{
+            color: `${toggleInput ? "#5b06a7" : "#5f82d9"}`,
+          }}
+        >
+          Contact me
+        </strong>
         <p>
           Write your best email
           <br />
@@ -30,7 +44,13 @@ function Contact() {
         <SocialMedias />
       </div>
       <div className="form">
-        <input required />
+        <input
+          onFocus={() => handleToggle()}
+          onBlur={() => handleToggle()}
+          onInvalid={() => handleToggle()}
+          // onFocusOut={() => console.log("out")}
+          required
+        />
         <label>Your Email</label>
 
         <MailOutlineIcon style={emailStyle} />
